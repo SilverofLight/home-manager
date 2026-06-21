129f8d45c9a39e8beca28c4ffdac21addd5ec761
SilverofLight <a2061686759@outlook.com>
2026-06-21 19:17:48 +0800

    check after switch

--- home.nix
+++ home.nix
@@ -1,11 +1,11 @@
-{ config, pkgs, nixGL, ... }:
+{ config, pkgs, lib, ... }:
 
 {
   home.username = "silver";
   home.homeDirectory = "/home/silver";
   targets.genericLinux.gpu.enable = true;
 
-  home.stateVersion = "25.05";
+  home.stateVersion = "26.11";
 
   imports = [
     ./modules/cli/git.nix
@@ -30,6 +30,41 @@
     ./modules/desktop/wallpaper.nix
     ./modules/desktop/mouseless.nix
   ];
+
+  # 每次 switch 之后自动执行的命令
+  home.activation.myScript =
+    lib.hm.dag.entryAfter [ "writeBoundary" ] ''
+      echo "home-manager activated"
+      check_cmd() {
+        local cmd="$1"
+  
+        if command -v "$cmd" >/dev/null 2>&1; then
+          echo "[OK] command found: $cmd"
+        else
+          printf '\033[30;43m[WARNING]\033[0m command not found: %s\n' "$cmd"
+        fi
+      }
+  
+      check_service() {
+        local service="$1"
+  
+        if [ -f "/etc/systemd/system/$service" ]; then
+          echo "[OK] service found: $service"
+        else
+          printf '\033[30;43m[WARNING]\033[0m service missing: %s\n' "$service"
+        fi
+      }
+  
+      echo "===== Home Manager Environment Check ====="
+  
+      check_cmd niri
+      check_cmd qutebrowser
+  
+      check_service mouseless.service
+      check_service singbox.service
+  
+      echo "=========================================="
+    '';
 
   programs.home-manager.enable = true;
 }
