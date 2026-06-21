d02f523b0a80e21dc6b0978bd403bd0ef9f2d6d5
SilverofLight <a2061686759@outlook.com>
2026-06-21 19:50:33 +0800

    separation activation-check

--- home.nix
+++ home.nix
@@ -32,55 +32,9 @@
     ./modules/desktop/mouseless.nix
     ./modules/desktop/zathura.nix
     ./modules/desktop/swayimg.nix
+
+    ./modules/activation-check.nix
   ];
-
-  # 每次 switch 之后自动执行的命令
-  home.activation.myScript =
-    lib.hm.dag.entryAfter [ "writeBoundary" ] ''
-      echo "home-manager activated"
-      check_cmd() {
-        local cmd="$1"
-  
-        if command -v "$cmd" >/dev/null 2>&1; then
-          echo "[OK] command found: $cmd"
-        else
-          printf '\033[30;43m[WARNING]\033[0m command not found: %s\n' "$cmd"
-        fi
-      }
-  
-      check_service() {
-        local service="$1"
-  
-        if [ -f "/etc/systemd/system/$service" ]; then
-          echo "[OK] service found: $service"
-        else
-          printf '\033[30;43m[WARNING]\033[0m service missing: %s\n' "$service"
-        fi
-      }
-
-      check_non_nixos_gpu() {
-        if [ -L /etc/tmpfiles.d/non-nixos-gpu.conf ]; then
-          echo "[OK] non-nixos-gpu-setup has been run"
-        else
-          printf '\033[30;43m[WARNING]\033[0m run: sudo non-nixos-gpu-setup\n'
-        fi
-      }
-  
-      echo "=========================================="
-      echo "===== Home Manager Environment Check ====="
-      echo "=========================================="
-  
-      check_cmd niri
-      check_cmd qutebrowser
-  
-      check_service mouseless.service
-      check_service singbox.service
-
-      check_non_nixos_gpu
-  
-      echo "=========================================="
-      echo "=========================================="
-    '';
 
   programs.home-manager.enable = true;
 }
